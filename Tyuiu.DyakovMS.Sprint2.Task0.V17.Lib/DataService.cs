110acd3c4972c87ac38d79db282294d9f1cd3a52
dyakovmaksim <pochta3478@mail.ru>
2024-10-04 23:10:50 +0500

    Add project files.

---
+++ Tyuiu.DyakovMS.Sprint2.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.DyakovMS.Sprint2.Task0.V17.Lib
+{
+    public class DataService : ISprint2Task0V17
+    {
+
+    }
+}
---
+++ Tyuiu.DyakovMS.Sprint2.Task0.V17/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.DyakovMS.Sprint2.Task0.V17.Lib;
+namespace Tyuiu.DyakovMS.Sprint2.Task0.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Дьяков Максим Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Дьяков Максим Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений и арифметических выражений, кото*");
+            Console.WriteLine("*орая вернет логическую последовательность.                               *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
